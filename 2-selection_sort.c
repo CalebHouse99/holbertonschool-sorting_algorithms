eb01a54319c634b07e0d6ba981b4240e8001166d
CalebHouse99 <calebhouse99@gmail.com>
2022-08-17 22:00:46 -0700

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap - swaps two pointers.
+ * @xp: first pointer.
+ * @yp:  second pointer.
+ *
+ * Return: void
+ */
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
 * selection_sort - selection sort algrithm
 *
 *@array: Array
