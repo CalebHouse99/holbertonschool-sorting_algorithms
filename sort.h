246ca736f26ca589bed25fe15890ff0a34df2b7b
Caleb House <calebhouse99@gmail.com>
2023-03-01 21:03:14 -0800

    betty

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,8 +7,8 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @prev: Pointer to the previous element
- * @next: Pointer to the next element
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
